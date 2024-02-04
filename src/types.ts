707ef7abae0395b9d3693069727807b535d52ac3
Kevin Zhu <kevin.yd.zhu@gmail.com>
2024-02-05 10:47:31 +1100

    feat: working summary dashboard

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,46 @@ export const defaultQuery: Partial<MyQuery> = {
   constant: 6.5,
 };
 
+export interface Vulnerability {
+  // "id": "01HNJ8W4AVFS53A61HSPZDB9CS",
+  // "title": "Ensure S3 bucket has block public policy enabled",
+  // "severity": "MEDIUM",
+  // "language": "CloudFormation",
+  // "message": "",
+  // "filePath": "cloudformation/insecure.yaml",
+  // "cwe": 0,
+  // "ruleId": "CKV_AWS_54",
+  // "ruleUrl": "",
+  // "startLine": 5,
+  // "endLine": 19,
+  // "isAutoFixable": false,
+  // "suggestions": null,
+  // "exampleFixes": null,
+  // "isAllowlisted": false,
+  // "latest": true
+  id: string;
+  title: string;
+  severity: string;
+  language: string;
+  message: string;
+  filePath: string;
+  cwe: number;
+  ruleId: string;
+  ruleUrl: string;
+  startLine: number;
+  endLine: number;
+  isAutoFixable: boolean;
+  suggestions?: any[];
+  exampleFixes?: any[];
+  isAllowlisted: boolean;
+  latest: boolean;
+}
+
+export interface NullifySastSummaryApiResponse {
+  vulnerabilities: Vulnerability[];
+  numItems: number;
+}
+
 /**
  * These are options configured for each DataSource instance
  */
